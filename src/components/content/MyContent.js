c4c99a3790f3f75de1981710b7fbd65e76648271
Akemine <stephane.mestric@autoliv.com>
2020-07-22 11:47:41 +0200

    Border color random added Hover and unHover sur images added

--- src/components/content/MyContent.js
+++ src/components/content/MyContent.js
@@ -6,13 +6,22 @@ class MyContent extends Component {
 
           constructor(props){
                     super(props)
+
+                    this.state = {
+                        colors: [],
+                        color: ''
+                    }
           }
 
+          randomColorGenerator(min, max){
+              this.state.colors = ['orange', 'red', 'green', 'brown', 'cyan', 'pink', 'purple', 'black', 'yellow', 'pink', 'cornflowerblue'];
+              this.state.color = Math.floor(Math.random() * (max - min + 1) ) + min;
+              return this.state.colors[this.state.color]
+          }
           
           
     render() {
           
-
         return (
         
           <div className="myRounded d-flex justify-content-around scrollBarVertical" style={{flexWrap:'wrap'}} >
@@ -20,7 +29,7 @@ class MyContent extends Component {
                      
                      {this.props.images.data.map((myImg, index) => (
                                <div key={index}>
-                              <img className="imgRounded child " src={process.env.PUBLIC_URL + '/images/oeuvres/' + myImg} alt="luwImage" />
+                              <img style={{border: '3px solid ' +  this.randomColorGenerator(0, 10)}} className="imgRounded child" src={process.env.PUBLIC_URL + '/images/oeuvres/' + myImg} alt="luwImage" />
                               </div>
                      )) 
                     }
