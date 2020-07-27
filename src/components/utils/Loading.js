185b73396ce0f261decf4138ff8cdff900ea978b
Akemine <stephane.mestric@autoliv.com>
2020-07-27 11:27:58 +0200

    test

--- src/components/utils/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <div style={{ minHeight: '100vh' }} className="d-flex flex-row justify-content-center align-items-center w-100">
-      <img alt="loading gif" src={process.env.PUBLIC_URL + '/images/gif.gif' }/>
-    </div>
-  );
-}
--- src/conf/id.twitch.js
+++
@@ -1,7 +0,0 @@
-import * as axios from 'axios';
-
-const idTwitch = axios.create({
-          baseURL: 'https://id.twitch.tv'
-        })
-
-export default idTwitch;
